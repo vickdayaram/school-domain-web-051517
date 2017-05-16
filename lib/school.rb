87a32a361d763e4804998a8f21bc2eb454e3e807
github username <github email address>
2017-05-16 14:03:20 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
+
+
 # code here!
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    if roster[grade] == nil
+       roster[grade] = []
+       roster[grade] << student
+    elsif roster.has_key?(grade)
+       roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.each do |key, value|
+      roster[key] = value.sort
+    end
+    roster
+  end 
+end
